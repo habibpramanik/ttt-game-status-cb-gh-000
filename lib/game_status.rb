7d8e939216b4c94c2c7a0651d0760501e64c532a
Aamir Kalimi <habibpramanik1998@gmail.com>
2018-11-21 05:44:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,4 +24,10 @@ def won?
     position_2 = board[win_index_2] 
     position_3 = board[win_index_3]
   end
+  
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination # return the win_combination indexes that won.
+  else
+    false
+  end
 end
